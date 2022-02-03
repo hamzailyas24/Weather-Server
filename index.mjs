0d00d2d12e20c1c61b683d5db408136dcbcb2424
Hamza Ilyas <hamza24061@gmail.com>
2022-02-04 02:53:48 +0500

    Updated

--- index.mjs
+++ index.mjs
@@ -1,71 +1,39 @@
 import express, { request } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import dfff from "dialogflow-fulfillment";
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('short'));
+app.use(morgan("short"));
 
 app.use((req, res, next) => {
   console.log("a request came", req.body);
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // WEBHOOK
 
-app.post('/webhook', (req, res) => {
-    console.log('GOT a POST Request');
-    if(!req.body){
-        return res.sendStatus(400);
-    }
-    console.log('here is the post request from Dialogflow');
-    console.log(req.body);
-    console.log('Got geo city parameter from Dialogflow' + req.body.queryResult.parameters['geo-city']);
-    var city = req.body.queryResult.parameters['geo-city'];
-    var w = getWeather(city);
-    let response = " "; //Default response from the webhook to show that it is working.
-    let responseObj = {
-        "fulfillmentText": response,
-        "fulfillmentMessages": [{'text': {'text': [w]}}],
-        "source": ""
-    };
-    console.log('here is the reposnse to Dialogflow');
-    console.log(responseObj);
-    return res.json(responseObj);
+app.post("/webhook", (req, res) => {
+  const agent = new dfff.WebhookClient({ request: req, response: res });
+
+  function welcome(agent) {
+    agent.add("Sending Response from a Webhook Server to Dialogflow");
+  }
+
+  var intentMap = new Map();
+
+  intentMap.set("Default Welcome Intent", welcome);
+
+  agent.handleRequest(intentMap);
 });
 
-function cb(err, response, body) {
-
-    if(err) {
-        console.log('error:', error);
-    }
-
-    var weather = JSON.parse(body);
-    if(weather.message === 'city not found') {
-        result = 'unable to get weather' + weather.message;
-    } else {
-        result = 'Right now its ' + weather.main.temp + ' degrees with ' + weather.weather[0].description;
-    }
-
-}
-
-function getWeather(city) {
-    result = undefined;
-    var URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8bd6abdc20db1f463412a5c9df7dc7d7&units=metric`;
-    console.log(URL);
-    var req = request(URL, cb);
-    while(result === undefined) {
-        require('deasync').runLoopOnce();
-    }
-    return result;
-}
-
 app.listen(port, () => {
-  console.log(`Server is listening on port: ${port}`)
+  console.log(`Server is listening on port: ${port}`);
 });
